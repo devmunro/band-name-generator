fff6ef48db4b0d99c73f15e5adad9a1ecfa9132c
David Munro <defmunro@gmail.com>
2023-05-07 17:06:33 +0100

    created day1 band generator

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello to the Band Generator")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is your pets name?\n")
+print("Your band name could be " + city + " " + pet_name)
